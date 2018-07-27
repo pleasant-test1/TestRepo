a1e29268baa02df52509f1f1de9daf8c7bf8f5b1
Steffen Blake <Steffen.Blake.82@pleasantsolutions.com>
2018-07-27 15:11:15 -0600

    Initial Commit

---
+++ Source/Krate.Test.Tests/ThisShouldPass.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Krate.Test.Tests
+{
+    public class ThisShouldPass
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ Source/Krate.Test/Models/TestModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Krate.Test.Models
+{
+    public class TestModel
+    {
+    }
+}
